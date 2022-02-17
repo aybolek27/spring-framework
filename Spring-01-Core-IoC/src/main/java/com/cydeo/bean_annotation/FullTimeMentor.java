b0df9d82310f3d727c7f9bab78e96c11c70e791b
12816 <aybolek27@gmail.com>
2022-02-17 17:36:58 -0600

    bean annotation

---
+++ Spring-01-Core-IoC/src/main/java/com/cydeo/bean_annotation/FullTimeMentor.java
@@ -0,0 +1,9 @@
+package com.cydeo.bean_annotation;
+
+public class FullTimeMentor {
+
+    public void creatAccount(){
+        System.out.println("Full Time Mentor is created");
+    }
+
+}
---
+++ Spring-01-Core-IoC/src/main/java/com/cydeo/bean_annotation/PartTimeMentor.java
@@ -0,0 +1,10 @@
+package com.cydeo.bean_annotation;
+
+public class PartTimeMentor {
+
+
+    public void creatAccount(){
+        System.out.println("Part Time Mentor is created");
+    }
+
+}
