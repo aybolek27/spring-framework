bd1cc2f8aedc434874451fbe5ddfbfe00e9c266a
12816 <aybolek27@gmail.com>
2022-02-24 19:00:19 -0600

    spring boot demo

--- Spring-05-Core-BeanScope/src/main/java/com/cydeo/service/CommentService.java
+++ Spring-05-Core-BeanScope/src/main/java/com/cydeo/service/CommentService.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 //@Scope("prototype")
-@Lazy
+//@Lazy
 public class CommentService {
 
     private final CommentRepository commentRepository;
