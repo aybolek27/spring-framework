546a1582815c8166d70dccb6be617568a7e89d93
12816 <aybolek27@gmail.com>
2022-03-03 17:24:10 -0600

    spring mvc - Controller

---
+++ Spring-06-SpringBoot-Demo/src/main/java/com/cydeo/model/Comment.java
@@ -0,0 +1,14 @@
+package com.cydeo.model;
+
+
+import lombok.Data;
+
+@Data
+public class Comment {
+
+    private String author;
+    private String text;
+
+
+
+}
---
+++ Spring-06-SpringBoot-Demo/src/main/java/com/cydeo/service/CommentService.java
@@ -0,0 +1,32 @@
+package com.cydeo.service;
+
+import com.cydeo.model.Comment;
+import com.cydeo.proxy.CommentNotificationProxy;
+import com.cydeo.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+//@Scope("prototype")
+//@Lazy
+public class CommentService {
+
+    private final CommentRepository commentRepository;
+    private final CommentNotificationProxy commentNotificationProxy;
+
+
+
+
+    public CommentService(CommentRepository commentRepository, @Qualifier("PUSH") CommentNotificationProxy commentNotificationProxy) {
+        this.commentRepository = commentRepository;
+        this.commentNotificationProxy = commentNotificationProxy;
+        System.out.println("Hello");
+    }
+
+    public void publishComment(Comment comment){
+        commentRepository.storeComment(comment);
+        commentNotificationProxy.sendComment(comment);
+
+    }
+
+}
