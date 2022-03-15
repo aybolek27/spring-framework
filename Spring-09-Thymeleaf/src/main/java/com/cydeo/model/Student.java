6b8624fb39e4508fcf5966a8dbbf8ae2c11b58a1
12816 <aybolek27@gmail.com>
2022-03-15 18:39:06 -0500

    html items added

---
+++ Spring-09-Thymeleaf/src/main/java/com/cydeo/model/Student.java
@@ -0,0 +1,11 @@
+package com.cydeo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Student {
+    public Student(String firstName, String lastName, int numberBetween, String state) {
+    }
+}
