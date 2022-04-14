421052a84800bef409ba8190f7142cc675a52863
12816 <aybolek27@gmail.com>
2022-04-14 01:17:42 -0500

    spring mvc form creation and submission

--- Spring-09-Spring-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
@@ -5,6 +5,8 @@ import com.cydeo.model.Mentor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Arrays;
@@ -14,13 +16,21 @@ import java.util.List;
 @RequestMapping("/mentor")
 public class MentorController {
 
-    @GetMapping("/mentor-register")
+    @GetMapping("/register")
     public String showForm(Model model){
 
         model.addAttribute("mentor", new Mentor());
         List<String> batchList= Arrays.asList("JD1", "JD2", "EU1", "EU2", "B18", "B20");
         model.addAttribute("batchList", batchList);
         return "mentor/mentor-register";
+    }
+
+    @PostMapping("/confirm")
+    public String showForm2(@ModelAttribute("mentor") Mentor mentor, Model model){
+        System.out.println(mentor.toString());
+        return "mentor/mentor-confirmation";
+
+//        return "redirect:/mentor/register";
     }
 
 }
