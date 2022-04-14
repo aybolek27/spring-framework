a7269e5d52fba7a7d503443453e7c4bbcdedc967
12816 <aybolek27@gmail.com>
2022-04-14 00:36:48 -0500

    spring mvc form creation

--- Spring-09-Spring-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
@@ -4,14 +4,17 @@ package com.cydeo.controller;
 import com.cydeo.bootstarp.DataGenerator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/student")
 public class StudentController {
 
 
-    @RequestMapping("/register")
+//    @RequestMapping(value = "/register",method = RequestMethod.GET)
+    @GetMapping("/register")
     public String register(Model model){
 
         model.addAttribute("students", DataGenerator.createStudent());
@@ -20,7 +23,8 @@ public class StudentController {
     }
 
 
-    @RequestMapping("/welcome")
+//    @RequestMapping("/welcome")
+    @GetMapping("/welcome")
     public String info(){
 
         return "student/welcome";
