ee1e9a77fc949d199889ced20f08c97b6e310139
12816 <aybolek27@gmail.com>
2022-06-15 22:52:39 -0500

    table creation, columns, annotations

--- Spring-10-ORM-TableCreation/src/main/resources/data.sql
+++ Spring-10-ORM-TableCreation/src/main/resources/data.sql
@@ -1,2 +1,9 @@
-INSERT  INTO EMPLOYEE(id,name)
-VALUES(1, 'Mike Smith');
+
+
+
+-- INSERT  INTO EMPLOYEE(id,name)
+-- VALUES(1, 'Mike Smith');
+
+
+INSERT  INTO STUDENT(id, first_name, last_name, email)
+VALUES(1, 'Mike','Smith','mike@cydeo.com');
