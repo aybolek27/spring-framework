e8b0ff566f4c18fa64ef25f7a3dd9917e1ab70c7
12816 <aybolek27@gmail.com>
2022-07-03 01:13:40 -0500

    one to many and many to many relationship example

--- Spring-12-ORM-Mapping-OneTwoMany-ManyToMany/src/main/java/com/cydeo/bootstrap/DataGenerator.java
+++ Spring-12-ORM-Mapping-OneTwoMany-ManyToMany/src/main/java/com/cydeo/bootstrap/DataGenerator.java
@@ -1,26 +1,36 @@
 package com.cydeo.bootstrap;
 
-import com.cydeo.entity.Payment;
-import com.cydeo.entity.PaymentDetail;
+import com.cydeo.entity.*;
 import com.cydeo.enums.Status;
-import com.cydeo.repository.PaymentRepository;
+import com.cydeo.repository.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Arrays;
 
 @Component
 public class DataGenerator implements CommandLineRunner {
 
 
+    private final MerchantRepository merchantRepository;
 
     private final PaymentRepository paymentRepository;
 
-    public DataGenerator(PaymentRepository paymentRepository) {
-        this.paymentRepository = paymentRepository;
-    }
+    private final CustomerRepository customerRepository;
 
+    private final ItemRepository itemRepository;
+
+    private final CartRepository cartRepository;
+
+    public DataGenerator(MerchantRepository merchantRepository, PaymentRepository paymentRepository, CustomerRepository customerRepository, ItemRepository itemRepository, CartRepository cartRepository) {
+        this.merchantRepository = merchantRepository;
+        this.paymentRepository = paymentRepository;
+        this.customerRepository = customerRepository;
+        this.itemRepository = itemRepository;
+        this.cartRepository = cartRepository;
+    }
 
     @Override
     public void run(String... args) throws Exception {
@@ -35,8 +45,45 @@ public class DataGenerator implements CommandLineRunner {
 
         payment2.setPaymentDetail(paymentDetail2);
 
+
+        Merchant merchant1 = new Merchant("AmazonSubMerchant","M123",new BigDecimal("0.25"),new BigDecimal("3.25"),5);
+
+
+        Customer customer1=new Customer("Texas", "ayb@gmail.com","Andy", "Hudson","aybolek27");
+
+        payment1.setCustomer(customer1);
+        payment2.setCustomer(customer1);
+
+        customerRepository.save(customer1);
+
+        payment1.setMerchant(merchant1);
+        payment2.setMerchant(merchant1);
+
+
+        Item item1=new Item("Milk","M01");
+        Item item2=new Item("Sugar","S01");
+        Item item3=new Item("Bread","B01");
+
+        Cart cart1=new Cart();
+        Cart cart2=new Cart();
+
+        cart1.setItemList(Arrays.asList(item1,item2,item3));
+        cart2.setItemList(Arrays.asList(item1,item2));
+
+        itemRepository.save(item1);
+        itemRepository.save(item2);
+        itemRepository.save(item3);
+
+        cartRepository.save(cart1);
+        cartRepository.save(cart2);
+
+        merchantRepository.save(merchant1);
+
         paymentRepository.save(payment1);
         paymentRepository.save(payment2);
+
+
+
 
 
     }
