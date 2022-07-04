74f8f5b63512a6e237644ae4b6fc67f3c8d6d829
12816 <aybolek27@gmail.com>
2022-07-03 21:08:17 -0500

    queries

--- Spring-12-ORM-Mapping-OneTwoMany-ManyToMany/src/main/java/com/cydeo/repository/PaymentRepository.java
+++ Spring-12-ORM-Mapping-OneTwoMany-ManyToMany/src/main/java/com/cydeo/repository/PaymentRepository.java
@@ -4,4 +4,12 @@ import com.cydeo.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+
+    //method --- > sql ----- > derive query
+
+    //sql - @Query
+
+    //JPQL - @Query
+
+
 }
